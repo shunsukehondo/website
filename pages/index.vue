d703ae3166e14fb9f2c7cec9cc7628bf50622176
Shunsuke HONDO <shunsuke.hondo@gmail.com>
2019-05-26 14:56:17 +0900

    clean up

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,14 @@
 <template>
-  <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <div class="cards">
-      <Card
-        v-for="person in people"
-        :key="person.id"
-        :person="person"
-      ></Card>
-    </div>
-  </section>
+  <h1>Hello</h1>
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
-import { Person } from "~/types";
-import Card from "~/components/Card.vue"
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
 
-@Component({
-  components: {
-    Card
-  }
-})
-export default class extends Vue {
-  @State people!: Person
-}
+@Component
+export default class extends Vue {}
 </script>
 
 <style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-
-.cards {
-  display: flex;
-  flex-wrap: wrap;
-}
 </style>
